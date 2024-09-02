9d28798d1dcdabd57a08c6ae7c61fe7e2c1dc419
MRIGAANKSh <mrigaanksharma928@gmail.com>
2024-09-02 20:49:35 +0530

    css done

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar1 from "./Components/navbar/Navbar1";
 import LoginSignUp from "./Components/login/login";
-import FaceDetection from "./Components/scndpage/scndpage.1";
+import FaceDetection from "./Components/scndpage/scnd2";
 import "./App.css";
 
 function App() {
